baa708d724ce32dce5ed4986b0f1d8958eadac89
Trazo <Espindola.A.E@hotmail.com>
2025-11-25 16:13:18 -0600

    StaticInitializerBlock

---
+++ JAVA/hackerRank/src/io/trazo/exercises/easy/introduction/EndOfFile.java
@@ -0,0 +1,20 @@
+package io.trazo.exercises.easy.introduction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EndOfFile {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    String line;
+    int lineNumber = 0;
+
+    while ((line = bufferedReader.readLine()) != null){
+      lineNumber ++;
+      System.out.printf("%s %s\n",lineNumber,line);
+    }
+
+  }
+}
---
+++ JAVA/hackerRank/src/io/trazo/exercises/easy/introduction/StaticInitializerBlock.java
@@ -0,0 +1,35 @@
+package io.trazo.exercises.easy.introduction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StaticInitializerBlock {
+
+  private static int b;
+  private static int h;
+  private static boolean flag;
+
+  static {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    try {
+      b = Integer.parseInt(bufferedReader.readLine());
+      h = Integer.parseInt(bufferedReader.readLine());
+      if(b <= 0 || h <= 0){
+        System.out.printf("java.lang.Exception: Breadth and height must be positive");
+        flag = false;
+      }else{
+        flag = true;
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void main(String[] args) {
+    if(flag){
+      int area = b * h;
+      System.out.print(area);
+    }
+  }
+}
