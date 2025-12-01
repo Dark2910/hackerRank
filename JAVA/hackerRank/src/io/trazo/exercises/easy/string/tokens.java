9102f209a31a16c975706a53349bcde51ba0b6c0
Trazo <Espindola.A.E@hotmail.com>
2025-12-01 13:35:52 -0600

    tokens

---
+++ JAVA/hackerRank/src/io/trazo/exercises/easy/string/tokens.java
@@ -0,0 +1,35 @@
+package io.trazo.exercises.easy.string;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class tokens {
+  public static void main(String[] args) {
+    String regex = "[^A-Za-z]+";
+    Pattern pattern = Pattern.compile(regex);
+
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine().trim();
+
+    if (input.isBlank()) {
+      System.out.println(0);
+      return;
+    }
+
+    String[] tokens = pattern.split(input);
+
+    List<String> list = new ArrayList<>();
+    list.addFirst(String.valueOf(tokens.length));
+    for (String token : tokens) {
+      if (!token.isBlank()) {
+        list.add(token);
+      }
+    }
+
+    for(String x : list){
+      System.out.println(x);
+    }
+  }
+}
